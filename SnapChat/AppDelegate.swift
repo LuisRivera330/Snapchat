56dff59b02826183fb7ac8779b7f4051ae819a8a
LuisRivera330 <coartac.luis.rivera@gmail.com>
2022-06-03 14:15:00 -0500

    Laboratorio 11

--- SnapChat/AppDelegate.swift
+++ SnapChat/AppDelegate.swift
@@ -6,16 +6,31 @@
 //
 
 import UIKit
+import FirebaseCore
+
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate,GIDSignInDelegate{
+    
+
+    
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
+
         return true
     }
+    
+    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any])
+          -> Bool {
+            return GIDSignIn.sharedInstance().handle(url)
+        }
+    
+
 
     // MARK: UISceneSession Lifecycle
 
--- SnapChat/ViewController.swift
+++ SnapChat/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseAuth
 
-class ViewController: UIViewController {
 
+class iniciarSesionViewController: UIViewController{
+
+    
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
+    
 
+    
+    
+    @IBAction func iniciarSessionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password:passwordTextField.text!){(user,error) in
+            print("Intentando Iniciar Sesión")
+            
+            if error != nil{
+                print("Se presentó el siguiente error: \(error)")
+            }else{
+                print("Inicio de sesión exitoso")
+            }
+            
+        }
 
+    }
+    
+    
+    
 }
 
